662b11ccd66ba2dd661eaf03adb95359a5c8da36
victor <nilvictor99@gmail.com>
2025-09-23 09:49:06 -0500

    Add Role management functionality with CRUD operations and permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,12 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
-
+    use HasRoles;
     use SoftDeletes;
 
     /**
